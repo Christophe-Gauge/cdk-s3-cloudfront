b31c6887a7489a7bb194ccd1605d72b7f3009ca1
Christophe Gauge <chris@mountwhite.net>
2023-12-12 08:00:00 -0800

    Updated to OriginAccessIdentity

--- s3_cloudfront/s3_cloudfront_stack.py
+++ s3_cloudfront/s3_cloudfront_stack.py
@@ -1,3 +1,4 @@
+import typing as t
 from constructs import Construct
 from aws_cdk import (
     aws_iam as iam,
@@ -5,7 +6,7 @@ from aws_cdk import (
     aws_cloudfront as cloudfront,
     aws_cloudfront_origins as origins,
     aws_secretsmanager as secretsmanager,
-    Stack, Tags, aws_s3, RemovalPolicy, Duration, CfnOutput, CfnParameter
+    Stack, Tags, aws_s3, RemovalPolicy, Duration, CfnOutput, CfnParameter, Aws
 )
 
 
@@ -15,18 +16,11 @@ class S3CloudfrontStack(Stack):
         super().__init__(scope, id, **kwargs)
 
         site_domain_name = CfnParameter(self,
-                                        id='domain_name',
-                                        description="Domain Name",
-                                        default="static.example.com",
-                                        type="String",
-                                              ).value_as_string
-        
-        source_ip = CfnParameter(self,
-                                        id='source_ip',
-                                        description="Source IP Address",
-                                        default="***.***.***.***",
-                                        type="String",
-                                      ).value_as_string
+            id='domain_name',
+            description="Domain Name",
+            default="static.example.com",
+            type="String",
+                    ).value_as_string
         
         stack_name = self.stack_name
         Tags.of(self).add('Project', "Wagtail Images");
@@ -105,38 +99,77 @@ class S3CloudfrontStack(Stack):
             )
         )
 
-        myBucket.add_to_resource_policy(
-            iam.PolicyStatement(
-                sid="RestrictByIPAddress",
-                effect=iam.Effect.DENY,
-                actions=["s3:*Object"],
-                resources=[myBucket.bucket_arn,myBucket.arn_for_objects("*")],
-                principals=[iam.AnyPrincipal()],
-                conditions = {
-                  'NotIpAddress' : {
-                    'aws:SourceIp' : source_ip
-                  }
-                }
-            )
-        )
-
         certificate = cm.Certificate(self, "MyCertificate",
           domain_name=site_domain_name,
           certificate_name=site_domain_name,
           validation=cm.CertificateValidation.from_dns()
         )
 
+        cfn_origin_access_control = cloudfront.CfnOriginAccessControl(self, "MyCfnOriginAccessControl",
+            origin_access_control_config=cloudfront.CfnOriginAccessControl.OriginAccessControlConfigProperty(
+                name=site_domain_name,
+                origin_access_control_origin_type="s3",
+                signing_behavior="always",
+                signing_protocol="sigv4",
+
+                # the properties below are optional
+                description="CF S3 Origin Access Control"
+            )
+        )
+
         distribution = cloudfront.Distribution(
-            self,
-            "cloudfront_distribution",
+            self, "CloudFrontDistribution",
             default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(myBucket),
+                compress=True,
+                allowed_methods=cloudfront.AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+                cached_methods=cloudfront.CachedMethods.CACHE_GET_HEAD,
+                cache_policy=cloudfront.CachePolicy.CACHING_OPTIMIZED,
+                origin_request_policy=cloudfront.OriginRequestPolicy.CORS_S3_ORIGIN,
                 viewer_protocol_policy=cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                origin=origins.S3Origin(
+                    myBucket, origin_id='s3-static-frontend',
+                )
             ),
             domain_names=[site_domain_name],
             certificate=certificate,
+            http_version=cloudfront.HttpVersion.HTTP2,
+            enabled=True,
             default_root_object="index.html",
+            price_class=cloudfront.PriceClass.PRICE_CLASS_ALL,
+            error_responses=[
+                cloudfront.ErrorResponse(
+                    http_status=404, response_page_path="/index.html", response_http_status=200,
+                    ttl=Duration.seconds(60)),
+            ]
         )
+
+        # Override the default behavior to use OAC and not OAI
+        # Because CDK does not have full support for this feature yet
+        # Based on: https://github.com/aws/aws-cdk/issues/21771#issuecomment-1282780627
+        cf_cfn_distrib = t.cast(
+            cloudfront.CfnDistribution,
+            distribution.node.default_child,
+        )
+        cf_cfn_distrib.add_property_override(
+            'DistributionConfig.Origins.0.OriginAccessControlId',
+            cfn_origin_access_control.get_att('Id'),
+        )
+        cf_cfn_distrib.add_property_override(
+            'DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity',
+            '',
+        )
+
+        myBucket.add_to_resource_policy(iam.PolicyStatement(
+            effect=iam.Effect.ALLOW,
+            actions=['s3:GetObject', 's3:ListBucket'],
+            resources=[f'{myBucket.bucket_arn}/*', myBucket.bucket_arn],
+            principals=[iam.ServicePrincipal("cloudfront.amazonaws.com")],
+            conditions={
+                "StringEquals": {
+                    "aws:SourceArn": f"arn:aws:cloudfront::{Aws.ACCOUNT_ID}:distribution/{distribution.distribution_id}"
+                }
+            }
+        ))
 
         # Add stack outputs
         CfnOutput(
@@ -158,6 +191,11 @@ class S3CloudfrontStack(Stack):
             self,
             "DistributionId",
             value=distribution.distribution_id,
+        )
+        CfnOutput(
+            self,
+            "distributionDomainName",
+            value=distribution.distribution_domain_name,
         )
         CfnOutput(
             self,
